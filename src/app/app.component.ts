2197a3ef002054c17caf4a2c2a904b6910bfdcff
Malu <maluizafelix@gmail.com>
2019-04-04 16:18:29 -0300

    Usando itens do json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
+import { Task, Track } from './lista-componentes/componente.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,29 +11,31 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 export class AppComponent {
   title = 'angular-drag-drop';
 
-  todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
+  private tracks: Track[] = require('./lista-componentes/componentes.json');
 
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
+    /**
+   * An array of all track ids. Each id is associated with a `cdkDropList` for the
+   * track talks. This property can be used to connect all drop lists together.
+   */
+  get trackIds(): string[] {
+    return this.tracks.map(track => track.id);
+  }
 
-  drop(event: CdkDragDrop<string[]>) {
+  onTalkDrop(event: CdkDragDrop<Task[]>) {
+    // In case the destination container is different from the previous container, we
+    // need to transfer the given task to the target data array. This happens if
+    // a task has been dropped on a different track.
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
+  }
+
+  onTrackDrop(event: CdkDragDrop<Track[]>) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
   }
 }
